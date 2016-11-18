bd35f49bad533bb8032ea37a2b75e04e8efd325a
Caue Queiroz <cauenqueiroz@gmail.com>
2016-11-18 19:08:14 -0200

    Add new task

--- app.js
+++ app.js
@@ -9,6 +9,15 @@
             {id: 3, name: 'Do more stuff', complete: true},
             {id: 4, name: 'Do that again', complete: false},
         ];
+
+        this.new = {};
+
+        this.addTask = function() {
+            this.new.id = this.tasks[this.tasks.length-1].id + 1;
+
+            this.tasks.push(this.new);
+            this.new = {};
+        };
     });
 
 })();
